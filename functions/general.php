993b0f483e2d14f4646a0670c538bf47b29df8a2
MadMike450 <m2r4508@gmail.com>
2018-02-07 10:43:19 -0500

    login database name changed
    I changed the database name so I had to update the login credentials

--- functions/general.php
+++ functions/general.php
@@ -5,7 +5,7 @@ function db_connector(){
 	$servername = 'localhost';
 	$user       = 'root';
 	$password   = 'abc123';
-	$dbname     = 'qrusers';
+	$dbname     = 'test_db_ood_website';
 	
 	$conn = new mysqli($servername, $user, $password, $dbname) or die("ERROR in general.php db_connector function.  Unable to connect to the database");
 	return $conn;
