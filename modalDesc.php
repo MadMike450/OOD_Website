6c60646d68899912d1094524b508edcc9c89f9de
MadMike450 <m2r4508@gmail.com>
2018-02-13 10:04:38 -0500

    code detailing
    just detailing the code

--- modalDesc.php
+++ modalDesc.php
@@ -15,23 +15,20 @@ $query = "SELECT * FROM products WHERE productID = $productID";
 
 <div>
 	<?php 
-	if ($productRecord = $conn->query($query)){
-				while($product = $productRecord->fetch_assoc()){
-					
-					echo "<center><img class='img-responsive' src='".$product['imagePath']."'/>";
-					echo "<h3>".$product['title']."</h3>"; 
-					echo "<hr />";
-					echo "<p><span class='label label-warning'>Product ID: ".$product['productID']."</span></p>";
-					echo "<p><span class='label label-success'>Price: $".$product['price']."</span></p>";
-					echo "<p>".$product['shortDesc']."</p>"; 
-					echo "<hr />";
-					echo $product['longDesc'];
-					echo "</center>";
-					
-
-				}//end while
-			}
-			//$newsRecord->free();
-			mysqli_close($conn); // close database connection
-		?>
+		if ($productRecord = $conn->query($query)){
+			while($product = $productRecord->fetch_assoc()){
+				echo "<center><img class='img-responsive' src='".$product['imagePath']."'/>";
+				echo "<h3>".$product['title']."</h3>"; 
+				echo "<hr />";
+				echo "<p><span class='label label-warning'>Product ID: ".$product['productID']."</span></p>";
+				echo "<p><span class='label label-success'>Price: $".$product['price']."</span></p>";
+				echo "<p>".$product['shortDesc']."</p>"; 
+				echo "<hr />";
+				echo $product['longDesc'];
+				echo "</center>";
+			}//end while
+		}
+		//$newsRecord->free();
+		mysqli_close($conn); // close database connection
+	?>
 </div>
